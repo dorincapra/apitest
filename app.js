f7ae1d9bf9d6d585226e2fc0825604e7d3c79135
Dorin Capra <dorinandrei16@gmail.com>
2023-12-15 15:09:29 +0200

    first

--- app.js
+++ app.js
@@ -10,28 +10,28 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(morgan("combined"));
-app.use(express.static('static'));
+app.use(express.static("static"));
 const apiKey = process.env.OPENAI_API_KEY;
 const openai = new OpenAI(apiKey);
 
-let assistant_id;
+let assistant_id = "asst_EkKQrG6iiaYkPbzXY4MpcVjZ";
 
 // Create an Assistant
-async function createAssistant() {
-  const assistantResponse = await openai.beta.assistants.create({
-    name: "Professional Chinese Teacher", // adjust name as per requirement
-    instructions: "You are a professional Chinese Teacher capable of teaching Chinese, answering related questions, and creating original content.Always Behave Like Professional and friendly chinese teacher.",
-    tools: [{ type: "code_interpreter" }], // adjust tools as per requirement
-    model: "gpt-4-1106-preview", // or any other GPT-3.5 or GPT-4 model
-  });
-  assistant_id = assistantResponse.id;
-  console.log(`Assistant ID: ${assistant_id}`);
-}
+// async function createAssistant() {
+//   const assistantResponse = await openai.beta.assistants.create({
+//     name: "Professional Chinese Teacher", // adjust name as per requirement
+//     instructions: "You are a professional Chinese Teacher capable of teaching Chinese, answering related questions, and creating original content.Always Behave Like Professional and friendly chinese teacher.",
+//     tools: [{ type: "code_interpreter" }], // adjust tools as per requirement
+//     model: "gpt-4-1106-preview", // or any other GPT-3.5 or GPT-4 model
+//   });
+//   assistant_id = assistantResponse.id;
+//   console.log(`Assistant ID: ${assistant_id}`);
+// }
 
-createAssistant();
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  });
+// createAssistant();
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+//   });
 
 // Endpoint to handle chat
 app.post("/chat", async (req, res) => {
@@ -64,17 +64,20 @@ app.post("/chat", async (req, res) => {
     }
 
     // Display the Assistant's Response
-const messagesResponse = await openai.beta.threads.messages.list(threadId);
-const assistantResponses = messagesResponse.data.filter(msg => msg.role === 'assistant');
-const response = assistantResponses.map(msg => 
-  msg.content
-    .filter(contentItem => contentItem.type === 'text')
-    .map(textContent => textContent.text.value)
-    .join('\n')
-).join('\n');
+    const messagesResponse = await openai.beta.threads.messages.list(threadId);
+    const assistantResponses = messagesResponse.data.filter(
+      (msg) => msg.role === "assistant"
+    );
+    const response = assistantResponses
+      .map((msg) =>
+        msg.content
+          .filter((contentItem) => contentItem.type === "text")
+          .map((textContent) => textContent.text.value)
+          .join("\n")
+      )
+      .join("\n");
 
-res.json({ response });
-
+    res.json({ response });
   } catch (error) {
     console.error("Error processing chat:", error);
     res.status(500).json({ error: "Internal server error" });
